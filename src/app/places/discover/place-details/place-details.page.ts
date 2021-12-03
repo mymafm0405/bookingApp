395c289ce65a9d0df11bfba3fbeda4caf8c5eb6b
Mahmoud Yhya <mymafm0405@gmail.com>
2021-12-03 17:08:00 +0300

    edit something

--- src/app/places/discover/place-details/place-details.page.ts
+++ src/app/places/discover/place-details/place-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Place } from '../../place.model';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-details',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-details.page.scss'],
 })
 export class PlaceDetailsPage implements OnInit {
+  currentPlace: Place;
+  placeId: string;
 
-  constructor() { }
+  constructor(private placesService: PlacesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.placeId =  params.placeId;
+      }
+    )
+    this.currentPlace = this.placesService.getCurrentPlace(this.placeId);
   }
 
 }
