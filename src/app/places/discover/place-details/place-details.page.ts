0cb371735f7811b7b99222a256d21018a7276c9a
Mahmoud <mymafm0405@gmail.com>
2021-12-04 08:58:38 +0300

    some changes

--- src/app/places/discover/place-details/place-details.page.ts
+++ src/app/places/discover/place-details/place-details.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-place-details',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-details.page.scss'],
 })
 export class PlaceDetailsPage implements OnInit {
+  constructor(private navCtrl: NavController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onBook() {
+    this.navCtrl.navigateBack('/places/discover');
   }
-
 }
